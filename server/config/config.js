ebdd2f0f9dd9227d3e5e290d4ff47133b2ec7091
Jorge A. Betancur <jbetancur@elespectador.com>
2020-08-26 19:59:49 -0500

    ResServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,19 @@
 // Puerto
 // ==================================
 process.env.PORT = process.env.PORT || 3000;
+
+// ==================================
+// Entorno
+// ==================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27027/cafe';
+} else {
+  urlDB =
+    'mongodb+srv://jbetancur:hK3hSSqqETDij01p@cluster0.llua5.mongodb.net/cafe';
+}
+
+process.env.urlDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,39 +1,24 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/usuario', (req, res) => {
-  res.json('get Usuario');
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', (req, res) => {
-  const { body } = req;
-  if (body.nombre === undefined) {
-    res.status(400).json({
-      ok: false,
-      message: 'El nombre es necesario',
-    });
-  } else {
-    res.json({
-      body,
-    });
-  }
-});
+mongoose.connect(
+  process.env.urlDB,
+  { useNewUrlParser: true, useCreateIndex: true },
+  (err, res) => {
+    if (err) throw console.log(err);
 
-app.put('/usuario/:id', (req, res) => {
-  const id = req.params.id;
-  res.json({
-    id,
-  });
-});
-
-app.delete('/usuario', (req, res) => {
-  res.json('delete Usuario');
-});
+    console.log('Base de Datos ONLINE');
+  },
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escuchando puerto:${process.env.PORT}`);
